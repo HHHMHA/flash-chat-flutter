1465ee795b9e71e4f18443826e3327defb08757e
J2mF <mp4-12cs5@hotmail.com>
2020-10-26 19:13:13 +0300

    Set welcome_screen.dart as home and Add Navigation Logic to it

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flash_chat/router.dart';
+import 'package:flash_chat/screens/welcome_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:flash_chat/screens/login_screen.dart';
 
 void main() => runApp(FlashChat());
 
@@ -13,7 +13,7 @@ class FlashChat extends StatelessWidget {
           body1: TextStyle(color: Colors.black54),
         ),
       ),
-      initialRoute: LoginScreen.ROUTE_NAME,
+      initialRoute: WelcomeScreen.ROUTE_NAME,
       onGenerateRoute: Router.generateRoute,
     );
   }
--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,3 +1,5 @@
+import 'package:flash_chat/screens/login_screen.dart';
+import 'package:flash_chat/screens/registration_screen.dart';
 import 'package:flutter/material.dart';
 
 class WelcomeScreen extends StatefulWidget {
@@ -44,7 +46,7 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
                 borderRadius: BorderRadius.circular(30.0),
                 child: MaterialButton(
                   onPressed: () {
-                    //Go to login screen.
+                    Navigator.pushNamed(context, LoginScreen.ROUTE_NAME);
                   },
                   minWidth: 200.0,
                   height: 42.0,
@@ -62,7 +64,7 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
                 elevation: 5.0,
                 child: MaterialButton(
                   onPressed: () {
-                    //Go to registration screen.
+                    Navigator.pushNamed(context, RegistrationScreen.ROUTE_NAME);
                   },
                   minWidth: 200.0,
                   height: 42.0,
